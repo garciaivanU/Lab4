74ee6a71af3a75ea73e36dacc6e7398dbc065ca4
Your Name <you@example.com>
2026-05-01 05:49:25 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -140,7 +140,13 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
 // Si x no tiene hijo izquierdo se retorna el mismo nodo.
 
 TreeNode * minimum(TreeNode * x){
-    return NULL;
+    if (x == NULL) return NULL; // Por si las dudas, uno nunca sabe
+    TreeNode* nodoAux = x;
+    
+    while (1) {
+        if (nodoAux->left == NULL) return nodoAux;
+        nodoAux = nodoAux->left;
+    }
 }
 
 // 5.- Implemente la función void removeNode(TreeMap * tree, TreeNode* node). 
